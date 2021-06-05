7fcbd7b523d155656d401e35a99c46bf723b337e
hearjest <79370810+hearjest@users.noreply.github.com>
2021-06-05 18:02:40 -0400

    Updates

--- Bellerophon.js
+++ Bellerophon.js
@@ -1,8 +1,6 @@
 /* jshint esversion: 6 */
-const dotenv=require('dotenv');
-dotenv.config();
-const Discord=require('discord.js');
-const client=new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const prefix='!';
 
 client.on('ready', () => {
@@ -15,23 +13,30 @@ client.on('ready', () => {
 
     if(command=='!smile'){
       message.channel.send(':catStretchedSmile:');
+    } 
+
+    if(command==='!yorokobeshounen'||command==='!喜べ少年'){//Kirei w/ cross
+      const embed1 =  new Discord.MessageEmbed().setImage('https://i.makeagif.com/media/6-12-2015/nIA0kY.gif') //adding a image
+      message.channel.send({embed:embed1});
     }
-      
-   
-    
+
+    if(command==='!thepriestsmiles'){//Kirei w/ cross
+      const embed1 =  new Discord.MessageEmbed().setImage('https://i.kym-cdn.com/photos/images/facebook/001/881/881/84b.jpg') //adding a image
+      message.channel.send({embed:embed1});
+    }
+
+    if(command==='!mapotofu'){//Kirei w/ cross
+      const embed1 =  new Discord.MessageEmbed().setImage('https://i.pinimg.com/originals/d9/3c/cd/d93ccd28fedfc910f1670555e08320ca.gif') //adding a image
+      message.channel.send({embed:embed1});
+    }
 
     if(!message.member.roles.cache.has('850518741325971456')){
       message.channel.send('Fuck you^');
     }
-
-    if(command==='Fuck you'){
-      message.delete()
-      .catch(console.error);
-    }
   
   });
  
- client.login(process.env.TOKEN);
+ client.login("ODUwNDkwMDUyMTAxNDA2NzUx.YLqelQ.CNjPiN_Nj6lIeGHjfqzdq34vlCo");
 
 
 
