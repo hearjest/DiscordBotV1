40f44fb20538d01f2b19643519e221ce3e846314
hearjest <79370810+hearjest@users.noreply.github.com>
2021-06-06 19:44:21 -0400

    Renaming stuff, added a new command
    The command adds a reaction to the message. Renamed Bellerophon to Kirei Kotomine, I just like him.

--- DiscordBot.js
+++ DiscordBot.js
@@ -7,20 +7,20 @@ client.on('ready', () => {
     console.log('Logged in...');
   });
   
-  client.on('message', message => {
+  client.on('message', message => {//!
     const command=message.content;
     if (!command.startsWith(prefix) || message.author.bot) return;
 
-    if(command=='!smile'){
+    /*if(command=='!smile'){
       message.channel.send(':catStretchedSmile:');
-    } 
+    }*/ 
 
     if(command==='!yorokobeshounen'||command==='!喜べ少年'){//Kirei w/ cross
       const embed1 =  new Discord.MessageEmbed().setImage('https://i.makeagif.com/media/6-12-2015/nIA0kY.gif') //adding a image
       message.channel.send({embed:embed1});
     }
 
-    if(command==='!thepriestsmiles'){//Kirei w/ cross
+    if(command==='!smile'){//Kirei w/ cross
       const embed1 =  new Discord.MessageEmbed().setImage('https://i.kym-cdn.com/photos/images/facebook/001/881/881/84b.jpg') //adding a image
       message.channel.send({embed:embed1});
     }
@@ -30,14 +30,14 @@ client.on('ready', () => {
       message.channel.send({embed:embed1});
     }
 
-    if(!message.member.roles.cache.has('850518741325971456')){
+    if(command==='!reactionTest'){
+      message.react('850549716927250482');
+    }
+
+    if(!message.member.roles.cache.has('!850518741325971456')){//850518466518581271
       message.channel.send('Fuck you^');
     }
   
   });
  
  client.login("ODUwNDkwMDUyMTAxNDA2NzUx.YLqelQ.CNjPiN_Nj6lIeGHjfqzdq34vlCo");
-
-
-
-  
